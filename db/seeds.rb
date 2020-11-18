129d10f5438203ce9e106b2feea9113116a41cce
Sy Rashid <sy.rashid.11@gmail.com>
2020-11-18 09:22:08 +0100

    FEAT: Added a nice looking banner to our show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Clearing Pokemon World"
+
+if Rails.env.development?
+  Trainer.destroy_all
+end
+
+puts "Creating Trainers"
+ash = Trainer.create!(
+  name: "Ash Ketchum",
+  banner_url: "https://www.thefactsite.com/wp-content/uploads/2017/09/ash-ketchum-facts-pokemon.jpg"
+)
+
+gary = Trainer.create!(
+  name: "Gary Oak",
+  banner_url: "https://www.tvovermind.com/wp-content/uploads/2018/08/GarySquirtle.jpg"
+)
+
+brock = Trainer.create!(
+  name: "Brock",
+  banner_url: "https://static1.srcdn.com/wordpress/wp-content/uploads/2016/11/Brock-Pokemon-Series.jpg"
+)
+
+puts "Created #{Trainer.count} pokemon trainers!"
